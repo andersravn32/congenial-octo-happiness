736569c0ef46999fa38053309e4c85f184b5b646
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-01-24 12:25:01 +0100

    Add: Added caesar logic

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-const ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-const casear = {
-    encrypt(input){
+const caesar = {
+  encrypt(input) {
+    // Define result
+    let result = "";
 
-    },
+    // Loop through input value
+    input.split("").forEach((char) => {
+      // Loop through alfabet
+      ALFABET.forEach((newChar, index) => {
+        if (newChar.toLowerCase() == char.toLowerCase()) {
+          // Reset array count if exceeds length of ALFABET
+          if (index + 3 >= ALFABET.length) {
+            return (result += ALFABET[index + 3 - ALFABET.length]);
+          }
 
-    decrypt(input){
+          // Return result with added char
+          return (result += ALFABET[index + 3]);
+        }
+      });
+    });
 
-    }
-}
+    // Return end result
+    return result || null;
+  },
+
+  decrypt(input) {
+    // define result
+    let result = "";
+
+    input.split("").forEach((char) => {
+      ALFABET.forEach((newChar, index) => {
+        if (newChar.toLowerCase() == char.toLowerCase()) {
+          // Loopback on array, and return correct value
+          if (index - 3 < 0) {
+            return (result += ALFABET[index - 3 + ALFABET.length]);
+          }
+
+          // Return value based on array position
+          return (result += ALFABET[index - 3]);
+        }
+      });
+    });
+
+    return result;
+  },
+};
 
 const viginere = {
-    encrypt(input){
+  encrypt(input) {},
 
-    },
+  decrypt(input) {},
+};
 
-    decrypt(input){
-        
-    }
-}
+caesar.encrypt("Hello world");
+caesar.decrypt("ABCD");
