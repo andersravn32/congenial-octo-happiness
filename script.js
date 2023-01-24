2da61dbb833fb1daf1e07de4307c5762548a43d3
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-01-24 11:53:32 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const casear = {
+    encrypt(input){
+
+    },
+
+    decrypt(input){
+
+    }
+}
+
+const viginere = {
+    encrypt(input){
+
+    },
+
+    decrypt(input){
+        
+    }
+}
